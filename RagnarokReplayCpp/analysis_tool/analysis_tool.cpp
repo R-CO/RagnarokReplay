1e164d85043e61e7988f970305adc9f2235e6fc2
R-CO <daniel1820kobe@gmail.com>
2021-04-29 17:15:36 +0800

    [feat] Dump chunks to it's own file
    1. Let analysis_tool dump each chunk to it's own file.
    
    2. More notes about parsing RO replay file.

--- RagnarokReplayCpp/analysis_tool/analysis_tool.cpp
+++ RagnarokReplayCpp/analysis_tool/analysis_tool.cpp
@@ -2,23 +2,96 @@
  * Author: R-CO
  * E-mail: daniel1820kobe@gmail.com
  **/
+#include <array>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
-using std::cout;
-using std::endl;
 #include <string>
+using namespace std;
 
+#include <direct.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "ChunkContainer.hpp"
 #include "Replay.hpp"
+using namespace RagnarokReplay;
+
+std::array<std::string, static_cast<size_t>(ContainerType::LastContainerType)>
+    g_container_type_str = {u8"None",           u8"PacketStream",
+                            u8"ReplayData",     u8"Session",
+                            u8"Status",         u8"Undefined_1",
+                            u8"Quests",         u8"GroupAndFriends",
+                            u8"Items",          u8"UnknownContainingPet",
+                            u8"Unknown_10",     u8"Undefined_2",
+                            u8"Unknown_12",     u8"Unknown_13",
+                            u8"InitialPackets", u8"Unknown_15",
+                            u8"Unknown_16",     u8"Efst",
+                            u8"Unknown_18",     u8"Unknown_19",
+                            u8"Unknown_20",     u8"Unknown_21",
+                            u8"Unknown_22",     u8"Unknown_23",
+                            u8"Unknown_24"};
+
+const string g_root_dir_path = u8"output";
+
+bool isDirExist(const string &dir_path);
+
+bool createOuputDirectories();
 
 int main(int argc, char *argv[]) {
-  std::string file_path = "../210227.rrf";
+  string file_path = "210227.rrf";
 
   if (argc >= 2) {
     file_path = argv[1];
   }
 
-  RagnarokReplay::Replay replay;
+  if (!createOuputDirectories()) {
+    cerr << u8"createOuputDirectories() returns false" << endl;
+    return EXIT_FAILURE;
+  }
+
+  Replay replay;
   replay.LoadFile(file_path);
 
+  for (const auto &chunk_container : replay.chunk_container_array) {
+    const auto dir_path = g_root_dir_path + u8'/' +
+                          g_container_type_str[static_cast<size_t>(
+                              chunk_container.container_type)] +
+                          u8'/';
+    for (const auto &chunk : chunk_container.data) {
+      const auto output_file_path = dir_path + to_string(chunk.header) + u8'_' +
+                                    to_string(chunk.id) + u8'_' +
+                                    to_string(chunk.time);
+      ofstream output_file(output_file_path, ios::out | ios::binary);
+      output_file.write(reinterpret_cast<const char *>(chunk.data.data()),
+                        chunk.data.size());
+    }
+  }
+
   return EXIT_SUCCESS;
+}
+
+bool isDirExist(const string &dir_path) {
+  struct _stat info;
+  if (_stat(dir_path.c_str(), &info) == 0 && info.st_mode & _S_IFDIR) {
+    return true;
+  }
+  return false;
+}
+
+bool createOuputDirectories() {
+  _mkdir(g_root_dir_path.c_str());
+  if (!isDirExist(g_root_dir_path)) {
+    return false;
+  }
+
+  for (auto &sub_dir_name : g_container_type_str) {
+    const auto dir_path = g_root_dir_path + u8'/' + sub_dir_name;
+    _mkdir(dir_path.c_str());
+    if (!isDirExist(dir_path)) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- RagnarokReplayCpp/note.md
+++ RagnarokReplayCpp/note.md
@@ -1,5 +1,11 @@
 # Analysis Note
 
+tools:
+
+1. HxD Hex Editor
+
+2. Cutter
+
 ## Container Type
 
 notes about contanier type
@@ -16,6 +22,20 @@ if (chunk.header == RagnarokReplay::ChunkHeaderType::HEADER_ZC_SKILLINFO_LIST) {
 ```
 
 1. HEADER_ZC_SKILLINFO_LIST (271U) => replay owner's skill information list.
+
+#### Structure of packet
+
+It's just a demonstration. Not C++ standard.
+
+##### HEADER_ZC_SKILLINFO_LIST
+
+```cpp=
+struct HEADER_ZC_SKILLINFO_LIST_PACKET {
+  uint16_t header;
+  uint16_t packet_size;
+  uint8_t data[packet_size - sizeof(header) - sizeof(packet_size)];
+};
+```
 
 ### ReplayData
 
@@ -52,7 +72,19 @@ Range: 1000~1089 (to be confirmed)
 e.g.
 
 ```cpp=
-if (chunk.id == RagnarokReplay::ReplayOpCodes::EquippedItems) {
+if (chunk.id == RagnarokReplay::ReplayOpCodes::Level) {
+  // do something
+}
+```
+
+### Status
+
+Use enum class RagnarokReplay::ReplayOpCodes to classify the meaning of each chunk(entry?).
+Range: 2000~2032 (to be confirmed)
+e.g.
+
+```cpp=
+if (chunk.id == RagnarokReplay::ReplayOpCodes::Level) {
   // do something
 }
 ```
